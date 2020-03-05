1f614c0a87ee2f17a4f70cd031ab6f499196ff49
s.fassetta <s.fassetta@gt2i.com>
2020-03-05 14:01:57 +0100

    Refactor all design (in progress)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cardsRole = require('../public/js/cards.js').default;
+const cardsRole = require(__dirname + '/../public/js/cards.js').default;
 
 router.get('/', function(req, res, next) {
     res.render('index', { cardsRole });
